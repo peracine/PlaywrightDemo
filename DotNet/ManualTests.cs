5ba3abafd9cd2895100283173e6855943e18099c
EliteBook840 <EliteBook840@DESKTOP-RKNOR41>
2022-05-15 14:10:55 +0100

    DotNet code

---
+++ DotNet/ManualTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PlaywrightDotNet;
+
+public class ManualTests
+{
+    [Test]
+    public async Task Page_title_is_correct()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Firefox.LaunchAsync();
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://en.wikipedia.org/wiki/Harald_V_of_Norway");
+
+        StringAssert.Contains("Harald V of Norway", await page.TitleAsync());
+    }
+}
---
+++ DotNet/RunnerTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightDotNet;
+
+//cf: https://playwright.dev/dotnet/docs/test-runners#base-nunit-classes-for-playwright
+[Parallelizable(ParallelScope.Self)]
+public class RunnerTests : PageTest
+{
+    [Test]
+    public async Task Testing_DOM_by_using_WebFirst_assertion()
+    {
+        await Page.GotoAsync("https://en.wikipedia.org/wiki/Harald_V_of_Norway");
+
+        await Expect(Page.Locator("h1")).ToHaveTextAsync("Harald V of Norway");
+    }
+
+    [Test]
+    public async Task Testing_HttpStattus_by_using_assert()
+    {
+        var response = await Page.GotoAsync("https://en.wikipedia.org/wiki/Harald_V_of_Norway");
+
+        Assert.AreEqual(200, response.Status);
+    }
+}
